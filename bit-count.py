d8ef22c06ae203d356cbb1e1702adae0d530a187
mudyro <111380820+mudyro@users.noreply.github.com>
2022-09-02 13:34:53 +0200

    Update bit-count.py

--- bit-count.py
+++ bit-count.py
@@ -1,23 +1,27 @@
 #Problem 81 from CodeAbbey website
 
-def bit_count(repeatitions):
+def bit_count(repeatitions): #Enter how many numbers you want to check
 
     num_list = [] #list to collect the input
 
-    for _ in range(repeatitions):
+    for _ in range(repeatitions): # convert the numbers into binary
         
         x = int(input("Provide numbers to be bit-counted: "))
         num_list.append(bin(x)[2:])
     
-    print(f'Binary version of number list is {num_list}')
-    ones_list = []
-
-
-    for bins in num_list:
+    print(f'Binary version of number list is {num_list}') #Show the binary representation of the list
+    ones_list = [] 
+    
+    for bins in num_list: #now count how many ones are there in each number binary representation and collect them into list (dictionary prolly would be better as i now think)
         one_count = 0
         for number in bins:
             if number == "1":
                 one_count += 1
         ones_list.append(one_count)
-
-    return ones_list
+    
+    #actually lets make them dictionaries
+    pairs = []
+    for i in range(len(num_list)):
+        pair = {num_list[i]:ones_list[i]}
+        pairs.append(pair)
+    return pairs
